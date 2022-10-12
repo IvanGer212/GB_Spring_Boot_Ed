9389b2fc6afc32db02513be0058a3367d5338064
IvanGer212 <ComradeIvan88@gmail.com>
2022-10-12 11:52:27 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/ru/gb_spring_boot_ed/GbSpringBootEdApplication.java
@@ -0,0 +1,13 @@
+package com.geekbrains.ru.gb_spring_boot_ed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSpringBootEdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSpringBootEdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geekbrains/ru/gb_spring_boot_ed/GbSpringBootEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbrains.ru.gb_spring_boot_ed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSpringBootEdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
